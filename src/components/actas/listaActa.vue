abedb966ad855976f28fb812426a2a5fd90e8b7c
jorge405 <jorgepomsjhope7@gmail.com>
2025-07-23 16:26:38 -0400

    agregando la lista de actas agregar busqueda y paginacion

--- src/components/actas/listaActa.vue
+++ src/components/actas/listaActa.vue
@@ -1,36 +1,78 @@
 <script>
 import api from '../../services/apiServices.js';
 import { Notyf } from 'notyf';
-
-const notyf= Notyf();
+import CryptoJS from 'crypto-js';
+import Cookies from 'js-cookie';
+const notyf= new Notyf();
 
 
 export default{
     data(){
         return{
             listActas:null,
-            clave:''    
+            clave:'conteoElectoralmittril',
+            //currentPage:1,
+            searchActas:''
+            
         }
     },
+    mounted(){
+        
+        this.listarActas()
+    }, 
     methods:{
-        listActas(){
-            const decryptMunicipio= CryptoJS.AES.decrypt(Cookies.get('municipio'),this.clave).toString(CryptoJS.enc.Utf8);
+        /*goToPage(page) {
+             if (page >= 1 && page <= this.totalPages) {
+                this.currentPage= page;
+            }
+        }, */
+        listarActas(){
+            const decryptUsuario=CryptoJS.AES.decrypt(Cookies.get('usuario'),this.clave).toString(CryptoJS.enc.Utf8);
+            
+            
+                try {
+                api.get(`lista_permiso/${decryptUsuario}`)
+                .then(response=>{
+                    if (!response.data.entry==[]) {
+                        this.permiso=response.data.entry;
+                        const encryptMunicipio= CryptoJS.AES.encrypt(this.permiso[0].municipio.replace(/ /g, '-'),this.clave).toString();
+                        Cookies.set('municipio',encryptMunicipio)
+                    }else{
+                        return []
+                    }
+                })
+            } catch (error) {
+                console.log('ha ocurrido un error al obtener los permisos: ',error);
+            } 
+            
+            const decryptMunicipio=CryptoJS.AES.decrypt(Cookies.get('municipio'),this.clave).toString(CryptoJS.enc.Utf8);
+            
             try {
                 api.post('lista_acta',{
                     tipo:'municipio',
-                    nombre:''
+                    nombre:decryptMunicipio
                 })
-                .then(response=>{
-                    if (response.data.status==='ok') {
-                        this.listActas=response.data
-                    }else{
-                        notyf.error('Error al cargar las actas');
-                    }
+                .then(response=>{ 
+                        console.log(response.data)
+                        this.listActas=response.data.data
+                    
                 })
             } catch (error) {
                 console.log('error en el servidor:', error)    
             }
         }
+    },
+    computed:{
+        /*totalPages(){
+            if (!this.listRecinto) return 1;
+            // Calcula páginas según el filtro
+            const filtered = this.listRecinto
+                ? this.listRecinto.filter(r =>
+                    r.recinto.toLowerCase().includes(this.searchRecinto.toLowerCase())
+                  )
+                : [];
+            return Math.ceil(filtered.length / this.itemsPerPage);
+        } */
     }
 }
 </script>
@@ -38,10 +80,10 @@ export default{
 
 
 <template>
-<div class="flex flex-col items-center justify-center mx-2 mt-4 bg-gray-900 min-h-screen w-full">
+<div class="flex flex-col items-center justify-center mx-2 mt-4  w-full">
     <div class="overflow-hidden rounded-xl border-gray-800 bg-white/[0.03] mt-4">
-        <input type="text" v-model="searchRecinto" placeholder="Buscar recinto" class="bg-gray-400/70 p-2.5 font-Outfit rounded-lg mt-4 mb-2 mx-3">
-        <div v-if="!listRecinto" class="flex justify-center items-center py-10">
+        <input type="text" v-model="searchActas" placeholder="Buscar actas" class="bg-gray-400/70 p-2.5 font-Outfit rounded-lg mt-4 mb-2 mx-3">
+        <div v-if="!listActas" class="flex justify-center items-center py-10">
             <span class="text-white text-lg font-Outfit"> Cargando Actas...</span>
         </div>
         <div v-else class="max-w-sm overflow-x-visible custom-scrollbar">
@@ -49,33 +91,40 @@ export default{
                 <thead>
                     <tr class="border-b border-gray-700">
                         <th class="px-5 py-3 text-left w-3/11">
-                            <p class="font medium text-gray-500 text-sm text-theme-xs">Recintos</p>
+                            <p class="font medium text-gray-500 text-sm text-theme-xs">Municipio</p>
                         </th>
                         <th class="px-5 py-3 text-left w-3/11">
-                            <p class="font medium text-gray-500 text-sm text-theme-xs">Subir acta</p>
+                            <p class="font medium text-gray-500 text-sm text-theme-xs">Recinto</p>
                         </th>
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-700">
-                    <tr v-for="(rec,index) in paginatedRecinto" :key="index" class="border-t border-gray-800">
+                    <tr v-for="(ac,index) in listActas" :key="index" class="border-t border-gray-800">
                         <td class="px-5 py-4 ">
                             <div class="flex items-center gap-3">
                                 <div>
-                                    <span class="block font-light text-sm font-Outfit text-theme-sm text-white/90">{{ rec.recinto }}</span>
+                                    <span class="block font-light text-sm font-Outfit text-theme-sm text-white/90">{{ ac.municipio }}</span>
                                 </div>
                             </div>
                         </td>
                         <td class="px-5 py-4 ">
+                            <div class="flex items-center gap-3">
+                                <div>
+                                    <span class="block font-light text-sm font-Outfit text-theme-sm text-white/90">{{ ac.recinto }}</span>
+                                </div>
+                            </div>
+                        </td>
+                        <!--<td class="px-5 py-4 ">
                             <div class="flex items-center gap-3">
                                 <div class="border border-gray-700 rounded-full p-2 hover:bg-blue-600 cursor-pointer" @click="abrirModal(rec)">
                                     <i class="pi pi-upload text-white"></i>
                                 </div>
                             </div>
-                        </td>
+                        </td> -->
                     </tr>
                 </tbody>
             </table>
-            <div class="flex justify-center items-center mt-4 mb-4 space-x-2">
+            <!--<div class="flex justify-center items-center mt-4 mb-4 space-x-2">
                 <button
                     @click="goToPage(currentPage - 1)"
                     :disabled="currentPage === 1"
@@ -93,12 +142,12 @@ export default{
                 >
                     Siguiente
                 </button>
-            </div>
+            </div>-->
         </div>
     </div>
 
     <!-- Modal -->
-     <transition
+     <!--<transition
      enter-active-class="transition duration-300 ease-out"
      enter-from-class="opacity-0 scale-95"
      enter-to-class=" opacity-100 scale-100"
@@ -107,7 +156,7 @@ export default{
      leave-to-class=" opacity-0 scale-95">
         <div v-if="modalVisible" class="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
     <div class="bg-gray-900 flex flex-col items-center justify-center mt-5 mx-4 rounded-lg py-10 px-5 w-full max-w-md relative">
-        <!-- Icono X para cerrar -->
+        
         <button type="button" @click="cerrarModal" class="absolute top-4 right-4 text-gray-300 hover:text-red-500 text-2xl transition">
             <i class="pi pi-times"></i>
         </button>
@@ -158,7 +207,7 @@ export default{
         </form>
     </div>
         </div>
-</transition>
+</transition>-->
     
 </div>
 
